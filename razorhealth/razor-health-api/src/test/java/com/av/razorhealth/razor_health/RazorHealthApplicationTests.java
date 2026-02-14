5e6c8b49ec8fbd09f6814ff59027e60096b72241
apple-v9 <pashley0802@gmail.com>
2026-02-14 15:33:59 -0800

    add Springdoc OpenAPI - swagger UI

--- razorhealth/razor-health-api/src/test/java/com/av/razorhealth/razor_health/RazorHealthApplicationTests.java
+++ razorhealth/razor-health-api/src/test/java/com/av/razorhealth/razor_health/RazorHealthApplicationTests.java
@@ -3,7 +3,7 @@ package com.av.razorhealth.razor_health;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = RazorHealthApplicationTests.class)
 class RazorHealthApplicationTests {
 
 	@Test
